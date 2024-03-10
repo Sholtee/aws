4baf418e344492095d10ebbca5ddb3d54302f1a2
denes.solti <sodnaatx@gmail.com>
2024-03-10 19:02:45 +0100

    lambda-microservice I

---
+++ lambda-microservice/Deploy-Foundation.ps1
@@ -0,0 +1,31 @@
+#
+# Deploy-Foundation.ps1
+#
+# Author: Denes Solti
+#
+# Usage: Deploy-Foundation.ps1 -action [create|update] -app appName -region regionName -profile profileName
+#
+
+param(
+  [Parameter(Position=0, Mandatory=$true)]
+  [string]$action,
+
+  [Parameter(Position=1, Mandatory=$true)]
+  [string]$app,
+
+  [Parameter(Position=2, Mandatory=$true)]
+  [string]$region,
+
+  [Parameter(Position=3, Mandatory=$true)]
+  [string]$profile
+)
+
+$ErrorActionPreference = "Stop"
+
+aws cloudformation ${action}-stack `
+  --profile ${profile} `
+  --stack-name ${app}-foundation `
+  --region ${region} `
+  --template-body file://./foundation.yml `
+  --parameters "ParameterKey=app,ParameterValue=${app}"  "ParameterKey=deploymentId,ParameterValue=$(New-Guid)" `
+  --capabilities CAPABILITY_NAMED_IAM
---
+++ lambda-microservice/Deploy-Global.ps1
@@ -0,0 +1,28 @@
+#
+# Deploy-Global.ps1
+#
+# Author: Denes Solti
+#
+# Usage: Deploy-Global.ps1 -action [create|update] -app appName -profile profileName
+#
+
+param(
+  [Parameter(Position=0, Mandatory=$true)]
+  [string]$action,
+
+  [Parameter(Position=1, Mandatory=$true)]
+  [string]$app,
+
+  [Parameter(Position=2, Mandatory=$true)]
+  [string]$profile
+)
+
+$ErrorActionPreference = "Stop"
+
+aws cloudformation ${action}-stack `
+  --profile ${profile} `
+  --stack-name ${app}-globals `
+  --region us-east-1 `
+  --template-body file://./global.yml `
+  --parameters "ParameterKey=app,ParameterValue=${app}" `
+  --capabilities CAPABILITY_NAMED_IAM
