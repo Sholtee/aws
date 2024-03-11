68ee8907bd243f4a10d4d59fa1def3f24e6e4aed
denes.solti <sodnaatx@gmail.com>
2024-03-11 08:17:16 +0100

    lambda-microservice III

---
+++ lambda-microservice/signer_test/index.mjs
@@ -0,0 +1,54 @@
+/****************************************************
+ * index.mjs
+ *
+ * Author: Denes Solti
+ *****************************************************/
+'use strict';
+import {defaultProvider} from '@aws-sdk/credential-provider-node';
+import {SignatureV4} from '@aws-sdk/signature-v4';
+import {createHash, createHmac} from 'crypto';
+
+class Sha256 {
+  constructor(secret) {
+    this.hash = secret
+      ? createHmac('sha256', secret)
+      : createHash('sha256');
+  }
+  update(array) {
+    this.hash.update(array);
+  }
+  digest() {
+    return Promise.resolve(new Uint8Array(this.hash.digest().buffer));
+  }
+}
+
+const
+  {accessKeyId, secretAccessKey, sessionToken} = await defaultProvider()(),
+  sigV4 = new SignatureV4({
+    service: 'lambda',
+    region: 'us-east-1',
+    credentials: {accessKeyId, secretAccessKey, sessionToken},
+    sha256: Sha256
+  });
+
+(async function main() {
+  try {
+    const signed = await sigV4.sign(
+      {
+        method: 'POST',
+        headers: {
+          Host: 'test1234test4321.lambda-url.eu-west-2.on.aws'
+        },
+        path: '/',
+        protocol: 'https',
+        body: null
+      },
+      {
+        signingRegion: 'eu-west-2'
+      }
+    );
+    console.log(JSON.stringify(signed));
+  } catch (err) {
+    console.log(err);
+  }
+})();
