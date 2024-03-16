d0d561618a7f67c0168dc53f7b2a37fa43a5d3d4
denes.solti <sodnaatx@gmail.com>
2024-03-16 17:10:01 +0100

    lambda-microservice XI

--- lambda-microservice/service1/index.mjs
+++ lambda-microservice/service1/index.mjs
@@ -1,32 +1,38 @@
 'use strict';
 import {GetSecretValueCommand, SecretsManagerClient} from '@aws-sdk/client-secrets-manager';
-import {createConnection} from 'mysql2/promise';
+import {createPool} from 'mysql2/promise';
 
-const smClient = new SecretsManagerClient();
+const dbConnectionPool = await createDbConnectionPool();
 
 export async function handler(event, context, callback) {
   try {
-    const
-      {username: user, password} = JSON.parse(
-        (
-          await smClient.send(new GetSecretValueCommand({SecretId: process.env.DB_SECRET_ARN}))
-        ).SecretString
-      ),
-      connection = await createConnection({
-        host: process.env.DB_ENDPOINT,
-        user,
-        password,
-        database: process.env.DB_NAME,
-      }),
-      [results] = await connection.query('SELECT "Hello World From MySQL" as hello');
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    console.log('Echo "Hello World" via MySQL');
+    const queryResult = await dbConnectionPool.query('SELECT "Hello World From MySQL" as hello');
+    console.log(`Done: ${JSON.stringify(queryResult)}`);
+
+    console.log('Echo "Hello World" via HTTP POST');
+    const httpBody = await (await fetch('https://postman-echo.com/post', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/plain'
+      },
+      body: 'Hello World From postman-echo'
+    })).text();
+    console.log(`Done: ${httpBody}`);
 
     callback(null, {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(results[0].hello)
+      body: JSON.stringify({
+        hello1: queryResult[0][0].hello,
+        hello2: JSON.parse(httpBody).data
+      })
     });
+    console.log('Bye');
   } catch (err) {
     console.error(err);
     callback(null, {
@@ -37,4 +43,17 @@ export async function handler(event, context, callback) {
       body: 'Internal server error'
     });
   }
+}
+
+async function createDbConnectionPool() {
+  const
+    {SecretString} = await new SecretsManagerClient().send(new GetSecretValueCommand({SecretId: process.env.DB_SECRET_ARN})),
+    {username: user, password} = JSON.parse(SecretString);
+
+  return createPool({
+    host: process.env.DB_ENDPOINT,
+    user,
+    password,
+    database: process.env.DB_NAME
+  });
 }
