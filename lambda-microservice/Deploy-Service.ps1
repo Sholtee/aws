28ca81855b7ff5e0eba48266fa398d49b4a9dc21
denes.solti <sodnaatx@gmail.com>
2024-03-22 06:58:07 +0100

    introduce "provisioned concurrent execution" support

--- lambda-microservice/Deploy-Service.ps1
+++ lambda-microservice/Deploy-Service.ps1
@@ -3,7 +3,7 @@
 #
 # Author: Denes Solti
 #
-# Usage: Deploy-Service.ps1 -action [create|update] -app app-name -service service-name -profile profile-name -region region-name
+# Usage: Deploy-Service.ps1 -action [create|update] -app app-name -service service-name -profile profile-name -region region-name [-concurrent-executions 10]
 #
 
 param(
@@ -20,37 +20,71 @@ param(
   [string]$profile,
 
   [Parameter(Position=4, Mandatory=$true)]
-  [string]$region
+  [string]$region,
+
+  [Parameter(Position=5, Mandatory=$false)]
+  [int]$concurrentExecutions=0
 )
 
 $ErrorActionPreference = "Stop"
 
 $functionName = "${app}-${service}-lambda"
 
-aws cloudformation ${action}-stack `
+$stackId = $(aws cloudformation ${action}-stack `
   --profile ${profile} `
   --stack-name "${app}-${service}" `
   --region ${region} `
   --template-body "file://./${service}.yml" `
-  --parameters "ParameterKey=app,ParameterValue=${app}" "ParameterKey=functionName,ParameterValue=${functionName}" `
-  --capabilities CAPABILITY_NAMED_IAM
+  --parameters `
+    "ParameterKey=app,ParameterValue=${app}" `
+    "ParameterKey=functionName,ParameterValue=${functionName}" `
+    "ParameterKey=concurrentExecutions,ParameterValue=${concurrentExecutions}" `
+  --capabilities CAPABILITY_NAMED_IAM `
+  --output text `
+  --query 'StackId'`
+)
+if ($stackId) {
+  Write-Host "Updating the stack..."
+  aws cloudformation wait stack-${action}-complete --region ${region} --stack-name "${app}-${service}"
+}
 
-aws cloudformation wait stack-create-complete --region ${region} --stack-name "${app}-${service}"
+Write-Host "Deploying function code..."
 
 Start-Process -FilePath npm -ArgumentList install -WorkingDirectory .\${service} -NoNewWindow -Wait
 
 Compress-Archive -Path .\${service}\* -DestinationPath .\${service}.zip
-try
-{
+try {
   aws lambda update-function-code `
     --profile ${profile} `
     --function-name ${functionName} `
     --zip-file "fileb://./${service}.zip" `
     --no-cli-pager
-}
-finally
-{
+} finally {
   Remove-Item -Path .\${service}.zip -Force
 }
 
 aws lambda wait function-updated --profile ${profile} --function-name ${functionName}
+
+$versions = (
+  $(aws lambda list-versions-by-function --profile ${profile} --function-name ${functionName}) | ConvertFrom-Json
+).Versions
+if ($versions.Length -gt 1) {
+  Write-Host "Arming a new version..."
+  $newVersion = $(aws lambda publish-version `
+    --profile ${profile} `
+    --function-name ${functionName} `
+    --output text `
+    --query 'Version'`
+  )
+  Write-Host $newVersion
+
+  aws lambda update-alias `
+    --profile ${profile} `
+    --function-name ${functionName} `
+    --function-version ${newVersion} `
+    --name armed-version
+
+  aws lambda wait function-updated --profile ${profile} --function-name ${functionName}
+}
+
+Write-Host "All ok"
