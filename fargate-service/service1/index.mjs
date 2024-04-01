73588084bdf38f10243c1ed180e9bc27a4e5077c
denes.solti <sodnaatx@gmail.com>
2024-04-01 09:13:20 +0200

    fargate-service II

--- fargate-service/service1/index.mjs
+++ fargate-service/service1/index.mjs
@@ -9,9 +9,10 @@ import express from 'express'
 const app = express();
 
 app.get('/healthcheck', (req, res) => {
+  console.log('Healthcheck initiated');
   res.setHeader('content-type', 'application/json');
   res.status(200);
   res.send(JSON.stringify('ok'));
 });
 
-app.listen(1986);
+app.listen(parseInt(process.env.SERVICE_PORT));
